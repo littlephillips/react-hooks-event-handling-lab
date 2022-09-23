9f5ec4a1e144b31cde6aa9040bd2d0b12f12de8c
littlephillips <phillipkinuthia72@gmail.com>
2022-09-23 15:54:18 +0300

    Updated the Eyesonme component

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  function handleFocusEvent(){
+    console.log("Good!");
+  };  
+
+  function handleBlurEvent(){
+      console.log("Hey! Eyes on me!");
+    };
+
+
+  return (
+      <button onFocus={handleFocusEvent} onBlur={handleBlurEvent}>
+      Eyes on me
+    </button>
+    
+  )
+}
+
+export default EyesOnMe;
